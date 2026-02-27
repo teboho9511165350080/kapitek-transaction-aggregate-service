eff5c1bf3692db449654b9f8127d7c50c2149768
teboho lekeno <41581824+teboho9511165350080@users.noreply.github.com>
2026-02-27 09:21:01 +0000

    enabling h2-console on security

--- src/main/java/com/kapitek/aggregator/kapitek_transaction_aggregate_service/infrastructure/adaptor/input/security/SecurityConfiguration.java
+++ src/main/java/com/kapitek/aggregator/kapitek_transaction_aggregate_service/infrastructure/adaptor/input/security/SecurityConfiguration.java
@@ -29,6 +29,7 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtDecoder()))
